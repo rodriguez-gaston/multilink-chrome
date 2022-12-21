5876add585bef522c0f2ae271f8c679282a65421
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-12-21 09:30:42 -0300

    Remove initial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="w-full flex justify-center bg-cyan-300">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
   );
 }
 
